a7f28a7403c3589e829fab344f0d023b25f48a38
Kalju Lee <kaljuvlee@gmail.com>
2020-06-20 15:08:49 -0400

    [data]: number input styling and data format

--- src/components/TimeInput/index.js
+++ src/components/TimeInput/index.js
@@ -3,23 +3,41 @@ import moment from 'moment';
 import { StyleSheet } from 'react-native';
 import { Card, CardItem, Text, Item, Input } from 'native-base';
 import NumberInput from '../NumberInput';
+import {numberInputFontSize} from '../../constants/styleValues';
 
 function TimeInput(props) {
   const { label, value } = props;
+  const styles = StyleSheet.create({
+    container: { width: '100%', height: 'auto' },
+    input: {
+      flex: 2,
+    },
+    divider: {
+      flex: 1,
+    },
+    dividerText: {
+      fontSize: numberInputFontSize,
+    },
+  });
+  // console.group('timeINput value');
+  // console.log(value);
+  // console.groupEnd();
   return (
-    <Card style={{width: '100%', height: 'auto'}}>
+    <Card style={styles.container}>
       <CardItem header>
         <Text>{label}</Text>
       </CardItem>
-      <CardItem style={{flexDirection: 'row', margin: 5}} cardBody>
-        <Item style={{flex: 1}} underline>
-          <NumberInput placeholder={'HH'} />
+      <CardItem style={{ flexDirection: 'row', margin: 5 }} cardBody>
+        <Item style={styles.input} underline>
+          <NumberInput value={value.h} placeholder={'HH'} />
         </Item>
-        <Item style={{flex: 1}} underline>
-          <NumberInput placeholder={'MM'} />
+        <Text style={styles.dividerText}>:</Text>
+        <Item style={styles.input} underline>
+          <NumberInput value={value.m} placeholder={'MM'} />
         </Item>
-        <Item style={{flex: 1}} underline>
-          <NumberInput placeholder={'SS'} />
+        <Text style={styles.dividerText}>:</Text>
+        <Item style={styles.input} underline>
+          <NumberInput value={value.s} placeholder={'SS'} />
         </Item>
       </CardItem>
     </Card>

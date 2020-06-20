f9ceae7724a512e67ec2f21ee30e735384740636
Kalju Lee <kaljuvlee@gmail.com>
2020-06-20 10:07:26 -0400

    [data]: rename players to participants

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux';
 
 const getInitialState = () => {
   return {
-    players: [],
+    participants: [],
     roundTime: 'no round time',
     breakTime: 'no breakTime',
     roundCount: 0,

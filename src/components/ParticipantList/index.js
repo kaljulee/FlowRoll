4aaca76bdaf55971c3b8671d27131e181b15b91d
Kalju Lee <kaljuvlee@gmail.com>
2020-06-23 14:40:17 -0400

    [chore]: react-native-easy-grid

---
+++ src/components/ParticipantList/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Card, CardItem } from 'native-base';
+import { Text, FlatList, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  card: {
+    width: '90%',
+  },
+  emptyListText: {
+    color: 'grey',
+  },
+  item: {
+    width: '100%',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  list: {
+    width: '100%',
+  },
+});
+
+function ParticipantList(props) {
+  const { participants, headerText } = props;
+  console.log(participants);
+
+  const EmptyList = () => (
+    <Card>
+      <CardItem>
+        <Text style={styles.emptyListText}> nothing here! </Text>
+      </CardItem>
+    </Card>
+  );
+
+  return (
+    <FlatList
+      style={styles.list}
+      data={participants}
+      keyExtractor={(item) => item.id}
+      ListEmptyComponent={() => EmptyList()}
+      renderItem={({ item }) => {
+        return (
+          <Card style={styles.card}>
+            <CardItem style={styles.item}>
+              <Text>{item.name}</Text>
+            </CardItem>
+          </Card>
+        );
+      }}
+    />
+  );
+}
+
+export default ParticipantList;

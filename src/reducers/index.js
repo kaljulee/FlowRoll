2e03e275c94edf6966bc768e92fac2a197261f97
Kalju Lee <kaljuvlee@gmail.com>
2020-06-23 17:16:12 -0400

    [data]: reducer has default schedule

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,10 @@ import { combineReducers } from 'redux';
 import { types } from '../actions';
 import { createParticipant } from '../models/Participant';
 import _ from 'lodash';
+import {
+  createDefaultOrdering,
+  participantsByActive,
+} from '../helpers/ordering';
 
 const getInitialState = () => {
   const participants = [
@@ -11,14 +15,18 @@ const getInitialState = () => {
     createParticipant('Nikos', 4),
   ];
 
+  const activeParticipants = [1, 2];
+  const schedule = createDefaultOrdering(
+    participantsByActive(participants, activeParticipants).active,
+  );
   return {
     participants,
-    activeParticipants: [1, 2],
+    activeParticipants,
     roundTime: { h: 0, m: 6, s: 0 },
     breakTime: { h: 0, m: 0, s: 30 },
     roundCount: 24,
     currentRound: 0,
-    schedule: [],
+    schedule,
     estimatedTime: undefined,
   };
 };

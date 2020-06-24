860bc65e3532b835eefd6c2227e1abf4505f2135
Kalju Lee <kaljuvlee@gmail.com>
2020-06-24 11:32:53 -0400

    [component]: time keeping element

--- src/components/FlowRollContainer/index.js
+++ src/components/FlowRollContainer/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { Container } from 'native-base';
+import React, { useState, useEffect } from 'react';
+import { Container, Button, } from 'native-base';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
 
 function FlowRollContainer(props) {
-  const { children } = props;
-  return <Container>{children}</Container>;
+  const { children, roundTime } = props;
+
+  const [displayTime, setDisplayTime] = useState(0);
+  const [activeTimer, setActiveTimer] = useState(null);
+
+  function clearTimer() {
+    clearInterval(activeTimer);
+    setActiveTimer(null);
+    setDisplayTime(0);
+  }
+
+  useEffect(() => {
+    console.log('starting timer');
+    const interval = setInterval(() => setDisplayTime((t) => t + 1), 1000);
+    if (interval !== activeTimer) {
+      setActiveTimer(interval);
+    }
+    return () => {
+      setActiveTimer(null);
+      clearInterval(interval);
+    };
+  }, []);
+  console.log('displaytime render ' + displayTime);
+
+  return (
+    <Container>
+      <Text>{displayTime}</Text>
+      <Button onPress={() => clearTimer()}><Text>clear timer</Text></Button>
+      {children}
+    </Container>
+  );
 }
 
 const mapStateToProps = (state) => {

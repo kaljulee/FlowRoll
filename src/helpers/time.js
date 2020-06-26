75b62c8ed729340d799932ebfadd0f53a17dbf99
Kalju Lee <kaljuvlee@gmail.com>
2020-06-26 12:59:36 -0400

    [data]: pullTime helper

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -4,3 +4,25 @@ export function HourMinuteSecond(time) {
 }
 
 export const ZERO_TIME = { h: 0, m: 0, s: 0 };
+
+function pullTime(total, unit) {
+  if (total > unit - 1) {
+    const overflow = total % unit;
+    const trackedTime = total - overflow;
+    return { tracked: trackedTime / unit, untracked: overflow };
+  }
+  return { tracked: 0, untracked: total };
+}
+
+export function secondsToHMS(time) {
+  let untrackedTime = time;
+
+  const pulledHours = pullTime(untrackedTime, 3600);
+  const h = pulledHours.tracked;
+  untrackedTime = pulledHours.untracked;
+
+  const pulledMinutes = pullTime(untrackedTime, 60);
+  const m = pulledMinutes.tracked;
+  const s = pulledMinutes.untracked;
+  return { h, m, s };
+}

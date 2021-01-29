c2578cc504fa50a9e9431dafed670b29abd59948
Kalju Lee <kaljuvlee@gmail.com>
2021-01-28 20:16:37 -0500

    [model]: state has comepeleteRRCycle field, matchups etc updated on active and deactive

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { types } from '../actions';
 import { createParticipant } from '../models/Participant';
 import _ from 'lodash';
 import {
-  createCompleteCycle,
+  createCompleteRRCycle,
   createMatchUps,
   participantsByActive,
 } from '../helpers/ordering';
@@ -70,6 +70,24 @@ const roundToBreak = (duration) => {
 const mergeActiveParticipants = (oldParticipants, newParticipants) =>
   _.uniq([...oldParticipants, ...newParticipants]);
 
+const updateParticipantMatchUps = (participants, activeParticipants) => {
+  const update = {};
+  const sortedParticipants = participantsByActive(
+    participants,
+    activeParticipants,
+  );
+  update.matchUps = createMatchUps(sortedParticipants.active);
+  update.completeCyle = createCompleteRRCycle(
+    update.matchUps,
+    sortedParticipants.active,
+  );
+  // schedule defaults to same as rr cycle
+  update.schedule = update.completeCyle;
+  return update;
+};
+
+//////////////////////////
+
 const getInitialState = () => {
   const participants = [
     createParticipant('Kalju', 1),
@@ -84,7 +102,11 @@ const getInitialState = () => {
     activeParticipants,
   );
   const matchUps = createMatchUps(sortedParticipants.active);
-  const schedule = createCompleteCycle(matchUps, sortedParticipants.active);
+  const completeRRCycle = createCompleteRRCycle(
+    matchUps,
+    sortedParticipants.active,
+  );
+
   return {
     participants,
     nextParticipantID: 5,
@@ -94,13 +116,14 @@ const getInitialState = () => {
     roundCount: 2,
     currentRound: 0,
     status: STATUS.IDLE,
-    schedule,
+    schedule: completeRRCycle,
     // matchUps is currently the same as schedule
     matchUps,
     estimatedTime: undefined,
     startTimeStamp: undefined,
     endTimeStamp: undefined,
     timerDuration: undefined,
+    completeRRCycle,
   };
 };
 
@@ -148,17 +171,38 @@ const basicReducer = (state = getInitialState(), action) => {
         ...update,
       };
     case types.ACTIVATE_PARTICIPANTS:
-      return {
-        ...state,
+      // merge in new active participants
+      update = {
         activeParticipants: mergeActiveParticipants(
           state.activeParticipants,
           payload,
         ),
       };
-    case types.DEACTIVATE_PARTICIPANTS:
+      // update matchups
+      update = {
+        ...update,
+        ...updateParticipantMatchUps(
+          state.participants,
+          update.activeParticipants,
+        ),
+      };
       return {
         ...state,
+        ...update,
+      };
+    case types.DEACTIVATE_PARTICIPANTS:
+      // filter out deactivated participants
+      update = {
         activeParticipants: _.without(state.activeParticipants, ...payload),
+      };
+      // update matchups
+      update = {
+        ...update,
+        ...updateParticipantMatchUps(update),
+      };
+      return {
+        ...state,
+        ...update,
       };
     case types.SET_CURRENT_ROUND:
       return { ...state, currentRound: payload };
@@ -209,7 +253,7 @@ const basicReducer = (state = getInitialState(), action) => {
       return {
         ...state,
         ...update,
-      }
+      };
     case types.START_TIMER_RUN:
       update = startTimerRun(state.roundDuration);
       return {

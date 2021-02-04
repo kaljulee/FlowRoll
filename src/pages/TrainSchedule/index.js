3d167823226c5e74dd1d5da6e13257bc975a43f7
Kalju Lee <kaljuvlee@gmail.com>
2021-02-04 16:36:15 -0500

    [control]: fixed data flow for leg toggle list

--- src/pages/TrainSchedule/index.js
+++ src/pages/TrainSchedule/index.js
@@ -4,12 +4,41 @@ import { Text } from 'native-base';
 import { Grid, Col } from 'react-native-easy-grid';
 import LegManager from '../../components/LegManager';
 import TrainTracker from '../../components/TrainTracker';
+import { unscheduleLeg, addLegToSchedule, deleteLegType } from '../../actions';
 
 function TrainSchedule(props) {
+  const {
+    legs,
+    legTypes,
+    unscheduleLeg,
+    deleteLegType,
+    addLegToSchedule,
+  } = props;
+  const unscheduleAllOfType = (id) => {
+    console.log('would unschedule all of type ' + id);
+  };
+
+  const deleteType = (id) => {
+    console.log(`would delete type ${id}`);
+  };
+  const unschedule = (id) => {
+    unscheduleLeg({legs: [id]});
+  };
+  const scheduleDefault = (id) => {
+    addLegToSchedule({ legs: [{ legType: id }] });
+  };
+
   return (
     <Grid>
       <Col size={3} style={{ borderWidth: 5 }}>
-        <LegManager />
+        <LegManager
+          onPressAvailableLeg={scheduleDefault}
+          onPressActiveLeg={unschedule}
+          onLongPressActiveLeg={unscheduleAllOfType}
+          onLongPressAvailableLeg={deleteType}
+          schedule={legs}
+          available={legTypes}
+        />
       </Col>
       <Col size={1} style={{ borderWidth: 5 }}>
         <TrainTracker />
@@ -19,10 +48,17 @@ function TrainSchedule(props) {
 }
 
 const mapStateToProps = (state) => {
-  return {};
+  const {
+    basicReducer: { legTypes, trainSchedule },
+  } = state;
+  return { legTypes, legs: trainSchedule.legs };
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  addLegToSchedule,
+  unscheduleLeg,
+  deleteLegType,
+};
 
 export default connect(
   mapStateToProps,

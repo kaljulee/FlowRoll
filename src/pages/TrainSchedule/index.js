4ff70e5ccd978e207c521df20d7b77e91ab961de
Kalju Lee <kaljuvlee@gmail.com>
2021-02-05 18:14:51 -0500

    [view]: estimated total time displayed

--- src/pages/TrainSchedule/index.js
+++ src/pages/TrainSchedule/index.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Text, Button, Container } from 'native-base';
+import { Text, Button, Container, Card, CardItem } from 'native-base';
 import { Grid, Col, Row } from 'react-native-easy-grid';
 import LegManager from '../../components/LegManager';
 import TrainTracker from '../../components/TrainTracker';
 import { unscheduleLeg, addLegToSchedule, deleteLegType } from '../../actions';
 import AddLegTypeModal from '../../components/modals/AddLegTypeModal';
+import { hourMinuteSecond, sumLegDurations } from '../../helpers/time';
 
 function TrainSchedule(props) {
   const {
@@ -17,6 +18,10 @@ function TrainSchedule(props) {
   } = props;
 
   const [showAddLegModal, setShowAddLegModal] = useState(false);
+  const [hmsTotalTime, setHmsTotalTime] = useState(sumLegDurations(legs));
+  const [displayTotalTime, setDisplayTotalTime] = useState(
+    hourMinuteSecond(hmsTotalTime),
+  );
 
   const unscheduleAllOfType = (id) => {
     console.log('would unschedule all of type ' + id);
@@ -32,11 +37,17 @@ function TrainSchedule(props) {
     addLegToSchedule({ legs: [{ legType: id }] });
   };
 
+  useEffect(() => {
+    const newSum = sumLegDurations(legs);
+    setHmsTotalTime(newSum);
+    setDisplayTotalTime(hourMinuteSecond(newSum));
+  }, [legs]);
+
   return (
     <Container>
       <Grid>
         <Col size={3} style={{ borderWidth: 5 }}>
-          <Row>
+          <Row size={4}>
             <LegManager
               onPressAvailableLeg={scheduleDefault}
               onPressActiveLeg={unschedule}
@@ -46,10 +57,18 @@ function TrainSchedule(props) {
               available={legTypes}
             />
           </Row>
-          <Row>
+          <Row size={1}>
             <Button onPress={() => setShowAddLegModal(true)}>
               <Text>Add New Type</Text>
             </Button>
+          </Row>
+          <Row>
+            <Card>
+              <CardItem style={{ display: 'flex', flexDirection: 'column' }}>
+                <Text>{'Est. Run Time'}</Text>
+                <Text>{displayTotalTime}</Text>
+              </CardItem>
+            </Card>
           </Row>
         </Col>
         <Col size={1} style={{ borderWidth: 5 }}>

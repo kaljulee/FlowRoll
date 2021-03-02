71ec06ec044fceb550f0164a15554a9e6997f1c4
Kalju Lee <kaljuvlee@gmail.com>
2021-03-02 17:25:53 -0500

    [logic]: check if start time is fresh

--- src/logic/index.js
+++ src/logic/index.js
@@ -3,3 +3,17 @@ import moment from 'moment';
 export function getElapsedSeconds(startTime) {
   return moment().diff(startTime, 'seconds');
 }
+
+// check if startTime is new by comparing it to the endTime of the last interval
+export function freshStartTime(startTime, endTime) {
+  // no startTime is considered stale - might need to revisit this
+  // this must be checked before allowing no-endTime to default to fresh start
+  if (!startTime) {
+    return false;
+  }
+  // if no endTime is provided, startTime is considered new
+  if (!endTime) {
+    return true;
+  }
+  return startTime.isAfter(endTime);
+}

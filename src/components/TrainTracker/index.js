433e81331ab695deb4c12f1910225a82ae71f6f4
Kalju Lee <kaljuvlee@gmail.com>
2021-03-03 18:29:38 -0500

    [view]: train tracker displays data from route

--- src/components/TrainTracker/index.js
+++ src/components/TrainTracker/index.js
@@ -1,8 +1,63 @@
 import React from 'react';
-import { Text } from 'native-base';
+import { Text, List, ListItem } from 'native-base';
+import { View, FlatList, StyleSheet, SafeAreaView } from 'react-native';
 
 function TrainTracker(props) {
-  return <Text>Train Tracker</Text>;
+  const { route } = props;
+
+  const railStyle = {
+    position: 'absolute',
+    height: '100%',
+    width: 7,
+    backgroundColor: 'black',
+  };
+
+  const listStyles = StyleSheet.create({
+    container: {
+      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+      display: 'flex',
+      flexGrow: 1,
+      alignItems: 'center',
+    },
+  });
+
+  const renderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          borderWidth: 1,
+          borderColor: 'black',
+          backgroundColor: item.color,
+          width: 70,
+          height: 28,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 5,
+          marginBottom: 5,
+        }}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{ height: '100%', width: '100%' }}>
+      <View
+        style={{
+          ...railStyle,
+          right: 15,
+        }}
+      />
+      <View style={{ ...railStyle, right: 60 }} />
+      <FlatList
+        data={route}
+        renderItem={renderItem}
+        keyExtractor={(i) => i.id}
+        contentContainerStyle={listStyles.container}
+      />
+    </SafeAreaView>
+  );
 }
 
 export default TrainTracker;

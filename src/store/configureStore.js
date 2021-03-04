d041d255a2ebe0993d7f5736d8d4f3c233b0e638
Kalju Lee <kaljuvlee@gmail.com>
2021-03-03 19:18:19 -0500

    [model]: store uses thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,7 @@
-import {createStore} from 'redux';
-import {persistStore, persistReducer} from 'redux-persist';
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import AsyncStorage from '@react-native-community/async-storage'; // defaults to localStorage for web
 
 import rootReducer from '../reducers';
@@ -12,7 +14,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
-  let store = createStore(persistedReducer);
+  let store = createStore(persistedReducer, applyMiddleware(thunk));
   let persistor = persistStore(store);
-  return {store, persistor};
+  return { store, persistor };
 };

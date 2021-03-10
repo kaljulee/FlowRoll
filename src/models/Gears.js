112e4acb8e8e605caca883fceeaf6d99d8738d44
Kalju Lee <kaljuvlee@gmail.com>
2021-03-10 16:18:44 -0500

    [model]: engine phase colors

--- src/models/Gears.js
+++ src/models/Gears.js
@@ -1,6 +1,27 @@
+import { COLORS } from '../constants/styleValues';
+
 export const Gears = {
   NEUTRAL: 'NEUTRAL',
   FULL_CYCLE: 'FULL_CYCLE',
   TRUNCATE: 'TRUNCATE',
   SHRINK_TO_FIT: 'SHRINK_TO_FIT',
+};
+
+export function getPhaseColor(phase) {
+  const colors = PHASE_COLORS();
+  return colors[phase];
+}
+
+export const PHASES = {
+  ROUND: 'ROUND',
+  WARMUP: 'WARMUP',
+  COOLDOWN: 'COOLDOWN',
+};
+
+export const PHASE_COLORS = () => {
+  const colors = {};
+  colors[PHASES.ROUND] = COLORS.RED;
+  colors[PHASES.WARMUP] = COLORS.CREAM;
+  colors[PHASES.COOLDOWN] = COLORS.LIGHTBLUE;
+  return colors;
 };

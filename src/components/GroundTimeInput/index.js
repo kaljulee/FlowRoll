de39a9836b837053344f94712118675df09b9aba
Kalju Lee <kaljuvlee@gmail.com>
2021-03-17 17:21:33 -0400

    [control]: first draft phase time modal

--- src/components/GroundTimeInput/index.js
+++ src/components/GroundTimeInput/index.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { Grid, Row, Col } from 'react-native-easy-grid';
-import { Text } from 'native-base';
-import { TextInput } from 'react-native';
+import { Text, Button } from 'native-base';
+import { TextInput, View } from 'react-native';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import SecondSlider from '../SecondSlider';
 import { formatSecondsToDisplay } from '../../helpers/time';
+import { PHASES } from '../../models/Gears';
+import PhaseEditorModal from '../modals/PhaseEditorModal';
 
 function calculateGroundTime(w, r, c) {
   return w + r + c;
 }
 
 function PhaseColumn(props) {
-  const { value, title } = props;
+  const { value, title, openEditor } = props;
   return (
     <Col
       style={{
@@ -20,7 +22,7 @@ function PhaseColumn(props) {
         alignContent: 'center',
       }}>
       <PhaseTitle title={title} />
-      <PhaseInput value={value} />
+      <PhaseInput value={value} openEditor={openEditor} />
     </Col>
   );
 }
@@ -30,12 +32,12 @@ function PhaseTitle(props) {
 }
 
 function PhaseInput(props) {
+  const { openEditor } = props;
+  const value = formatSecondsToDisplay(props.value);
   return (
-    <TextInput
-      keyboardType={'numeric'}
-      textAlign={'center'}
-      value={props.value.toString()}
-    />
+    <Button onPress={openEditor}>
+      <Text>{`${value}`}</Text>
+    </Button>
   );
 }
 
@@ -47,12 +49,16 @@ function GroundTimeInput(props) {
   );
   const [warmUpValue, setWarmUpValue] = useState(warmUp);
   const [coolDownValue, setCoolDownValue] = useState(coolDown);
-  const [workValue, setWorkTimeValue] = useState(work);
+  const [workValue, setWorkValue] = useState(work);
+  const [phaseToEdit, setPhaseToEdit] = useState(undefined);
+  const [editValue, setEditValue] = useState(undefined);
+  const [showEditor, setShowEditor] = useState(false);
 
   useEffect(() => {
     const newTotalTime = calculateGroundTime(warmUp, work, coolDown);
     setCoolDownValue(coolDown);
-    setWorkTimeValue(work);
+    setWorkValue(work);
+    setWorkValue(work);
     setWarmUpValue(warmUp);
     setTotalTime(newTotalTime);
   }, [warmUp, work, coolDown]);
@@ -92,6 +98,24 @@ function GroundTimeInput(props) {
     });
   };
 
+  function onChangeWarmUp(arg) {
+    console.log('warmup');
+    console.log(arg);
+    setWarmUpValue(arg);
+  }
+
+  function onChangeWork(arg) {
+    console.log('work');
+    console.log(arg);
+    setWorkValue(arg);
+  }
+
+  function onChangeCoolDown(arg) {
+    console.log('cool');
+    console.log(arg);
+    setCoolDownValue(arg);
+  }
+
   const onChangeTotalTime = (time) => {
     const timeDiff = time - totalTime;
     let newWorkTime;
@@ -103,7 +127,7 @@ function GroundTimeInput(props) {
     if (timeDiff * -1 <= work) {
       newWorkTime = work + timeDiff;
       setTotalTime(time);
-      setWorkTimeValue(newWorkTime);
+      setWorkTime(newWorkTime);
       setPhaseTimes({ work: newWorkTime, coolDown, warmUp });
     } else {
       setTotalTime(time);
@@ -115,47 +139,123 @@ function GroundTimeInput(props) {
     onChangeTotalTime(arg);
   };
 
+  function setPhaseToChange({ phase, value }) {
+    console.log('in SPTC');
+    console.log(value);
+    setEditValue(value);
+    setPhaseToEdit(phase);
+    setShowEditor(true);
+  }
+
+  const onCloseTimeModal = useCallback(
+    (newTime) => {
+      console.log('in onClose with time ' + newTime);
+      console.log('phasetoedit is');
+      console.log(phaseToEdit);
+      if (newTime) {
+        switch(phaseToEdit) {
+          case PHASES.COOLDOWN:
+            setCoolDownValue(newTime);
+            break;
+          case PHASES.WARMUP:
+            setWarmUpValue(newTime);
+            break;
+          case PHASES.WORK:
+            setWorkValue(newTime);
+            break;
+          default:
+            break;
+        }
+      }
+      setEditValue(undefined);
+      setPhaseToEdit(undefined);
+      setShowEditor(false);
+    },
+    [setEditValue, setShowEditor, phaseToEdit],
+  );
+
+  const openEditor = useCallback(
+    (phase) => {
+      console.log('open editor phase');
+      console.log(phase);
+      if (phase === PHASES.WARMUP) {
+        console.log('phase is warmup');
+        setPhaseToChange({
+          phase,
+          value: warmUpValue,
+        });
+      } else if (phase === PHASES.WORK) {
+        setPhaseToChange({ phase, value: workValue });
+      } else if (phase === PHASES.COOLDOWN) {
+        setPhaseToChange({
+          phase,
+          value: coolDownValue,
+        });
+      }
+    },
+    [
+      workValue,
+      warmUpValue,
+      coolDownValue,
+      // setWorkValue,
+      // setCoolDownValue,
+      // setWarmUpValue,
+    ],
+  );
   return (
-    <Grid>
-      <Row size={1}>
-        <PhaseColumn
-          title={'warmup'}
-          value={formatSecondsToDisplay(warmUpValue)}
-        />
-        <PhaseColumn title={'work'} value={formatSecondsToDisplay(workValue)} />
-        <PhaseColumn
-          title={'cooldown'}
-          value={formatSecondsToDisplay(coolDownValue)}
-        />
-      </Row>
-      <Row size={1} style={{ justifyContent: 'center' }}>
-        <MultiSlider
-          values={[warmUpValue, Math.abs(totalTime - coolDownValue)]}
-          enabledOne={true}
-          enabledTwo={true}
-          min={0}
-          max={totalTime}
-          onValuesChange={onValuesChange}
-          onValuesChangeFinish={onValuesChangeFinish}
-        />
-      </Row>
-      <Row size={2} style={{ display: 'flex', justifyContent: 'center' }}>
-        <Text>{`Total Round Time ${formatSecondsToDisplay(totalTime)}`}</Text>
-        <SecondSlider
-          isVisible={true}
-          seconds={totalTime}
-          onValueChange={onSecondSliderChange}
-        />
-        {false && (
-          <TextInput
-            keyboardType={'number-pad'}
-            onChangeText={onChangeTotalTime}
-            textAlign={'center'}>
-            <Text>{totalTime.toString()}</Text>
-          </TextInput>
-        )}
-      </Row>
-    </Grid>
+    <View>
+      <Grid>
+        <Row size={1}>
+          <PhaseColumn
+            title={'warmup'}
+            value={warmUpValue}
+            openEditor={() => openEditor(PHASES.WARMUP)}
+          />
+          <PhaseColumn
+            title={'work'}
+            value={workValue}
+            openEditor={() => openEditor(PHASES.WORK)}
+          />
+          <PhaseColumn
+            title={'cooldown'}
+            value={coolDownValue}
+            openEditor={() => openEditor(PHASES.COOLDOWN)}
+          />
+        </Row>
+        <Row size={1} style={{ justifyContent: 'center' }}>
+          <MultiSlider
+            values={[warmUpValue, Math.abs(totalTime - coolDownValue)]}
+            enabledOne={true}
+            enabledTwo={true}
+            min={0}
+            max={totalTime}
+            onValuesChange={onValuesChange}
+            onValuesChangeFinish={onValuesChangeFinish}
+          />
+        </Row>
+        <Row size={2} style={{ display: 'flex', justifyContent: 'center' }}>
+          <Text>{`Total Round Time ${formatSecondsToDisplay(totalTime)}`}</Text>
+          <SecondSlider
+            isVisible={true}
+            seconds={totalTime}
+            onValueChange={onSecondSliderChange}
+          />
+          {false && (
+            <TextInput
+              keyboardType={'number-pad'}
+              onChangeText={onChangeTotalTime}
+              textAlign={'center'}>
+              <Text>{totalTime.toString()}</Text>
+            </TextInput>
+          )}
+        </Row>
+      </Grid>
+      <PhaseEditorModal
+        showEditor={showEditor}
+        value={editValue}
+        onClose={onCloseTimeModal}
+      />
+    </View>
   );
 }
 

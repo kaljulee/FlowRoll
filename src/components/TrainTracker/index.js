85c084d5714653419bad42ac35c5d46cef0c1192
Kalju Lee <kaljuvlee@gmail.com>
2021-03-18 19:21:52 -0400

    [view]: trainTracker has location and time options

--- src/components/TrainTracker/index.js
+++ src/components/TrainTracker/index.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'native-base';
+import { Text, Button } from 'native-base';
+import Icon from 'react-native-vector-icons/Entypo';
 import { View, FlatList, StyleSheet, SafeAreaView } from 'react-native';
 import { EMPTY_MAP } from '../../models/Location';
 import { Grid, Col } from 'react-native-easy-grid';
+import { formatSecondsToDisplay } from '../../helpers/time';
 
+////////////////////////////
+// constants
+const DISPLAY_TYPES = {
+  TIME: 'TIME',
+  LOCATION: 'LOCATION',
+  ROUTE: 'ROUTE',
+};
+const tieStyling = {
+  borderWidth: 1,
+  borderColor: 'grey',
+  width: 70,
+  height: 8,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: 2,
+  marginBottom: 2,
+};
+
+///////////////////////////
+// supporting components
 function RailCol() {
   return <Col size={1} style={{ backgroundColor: 'black' }} />;
 }
@@ -25,9 +48,71 @@ function TrainRails() {
   );
 }
 
+function TrackDisplaySwitch(props) {
+  const { trackDisplay, onSwitch } = props;
+  const iconStyle = {
+    color: 'grey',
+    fontSize: 25,
+    padding: 1,
+  };
+  const timeStyle = { ...iconStyle };
+  const locationStyle = { ...iconStyle };
+  if (trackDisplay === DISPLAY_TYPES.TIME) {
+    timeStyle.color = 'white';
+  }
+  if (trackDisplay === DISPLAY_TYPES.LOCATION) {
+    locationStyle.color = 'white';
+  }
+  return (
+    <Button onPress={onSwitch} style={{ justifyContent: 'space-around' }}>
+      <Icon style={timeStyle} name={'stopwatch'} />
+      <Icon style={locationStyle} name={'image'} />
+    </Button>
+  );
+}
+
+function LocationTie(props) {
+  const { id, name, color, localTime, isSelected, runTime } = props;
+  return (
+    <View
+      style={{
+        ...tieStyling,
+        backgroundColor: isSelected ? 'green' : color,
+        height: 28,
+        marginTop: 5,
+        marginBottom: 5,
+      }}>
+      <Text>{formatSecondsToDisplay(runTime)}</Text>
+    </View>
+  );
+}
+
+function TimeTie(props) {
+  const { id, color, isSelected } = props;
+  return (
+    <View
+      style={{
+        ...tieStyling,
+        backgroundColor: isSelected ? 'green' : color,
+      }}
+    />
+  );
+}
+
+//////////////////////////////
+// export component
 function TrainTracker(props) {
   const { map, location, localTime } = props;
   const [selectedID, setSelectedID] = useState(0);
+  const [trackDisplay, setTrackDisplay] = useState(DISPLAY_TYPES.TIME);
+
+  function toggleTrackDisplay() {
+    if (trackDisplay === DISPLAY_TYPES.TIME) {
+      setTrackDisplay(DISPLAY_TYPES.LOCATION);
+    } else {
+      setTrackDisplay(DISPLAY_TYPES.TIME);
+    }
+  }
 
   const listStyles = StyleSheet.create({
     container: {
@@ -43,22 +128,31 @@ function TrainTracker(props) {
   };
 
   const renderItem = ({ item }) => {
+    if (trackDisplay === DISPLAY_TYPES.TIME) {
+      const tracks = [];
+      for (let i = 0; i < item.runTime; i += 300) {
+        tracks.push(
+          <TimeTie
+            key={`${item.id}_${i}`}
+            isSelected={checkIfSelected(item.id)}
+            id={item.id}
+            name={item.name}
+            color={item.color}
+          />,
+        );
+      }
+      return <View>{tracks}</View>;
+    }
     return (
-      <View
-        style={{
-          borderWidth: 1,
-          borderColor: 'black',
-          backgroundColor: checkIfSelected(item.id) ? 'green' : item.color,
-          width: 70,
-          height: 10,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 5,
-          marginBottom: 5,
-        }}>
-        <Text>{checkIfSelected(item.id) ? localTime : item.name}</Text>
-      </View>
+      <LocationTie
+        key={`${item.id}`}
+        localTime={localTime}
+        isSelected={checkIfSelected(item.id)}
+        id={item.id}
+        name={item.name}
+        color={item.color}
+        runTime={item.runTime}
+      />
     );
   };
 
@@ -83,7 +177,11 @@ function TrainTracker(props) {
         renderItem={renderItem}
         keyExtractor={(i) => `${i.id}`}
         contentContainerStyle={listStyles.container}
-        extraData={{ selectedID, localTime }}
+        extraData={{ selectedID, localTime, trackDisplay }}
+      />
+      <TrackDisplaySwitch
+        trackDisplay={trackDisplay}
+        onSwitch={toggleTrackDisplay}
       />
     </SafeAreaView>
   );

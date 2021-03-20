7df52496523cf79a155b84d5265ce1b88363b767
Kalju Lee <kaljuvlee@gmail.com>
2021-03-20 12:51:51 -0400

    [model]: floorStates are arrays of matchupIDs, not a single one

--- src/models/Segment.js
+++ src/models/Segment.js
@@ -1,15 +1,9 @@
-export function createSegment({
-  runTime,
-  routeType,
-  id,
-  cycleMatchUpIDs,
-  phase,
-}) {
+export function createSegment({ runTime, routeType, id, matchUpIDs, phase }) {
   return {
     runTime,
     routeType,
     id,
     phase,
-    cycleMatchUpIDs,
+    matchUpIDs,
   };
 }

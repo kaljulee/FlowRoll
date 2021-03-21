aa873943b26e162ced6e15648f533e434724bc87
Kalju Lee <kaljuvlee@gmail.com>
2021-03-21 17:59:04 -0400

    [dev]: tieTypes exports tie selector and getTieDimensions

--- src/components/TrainTracker/tieTypes.js
+++ src/components/TrainTracker/tieTypes.js
@@ -11,16 +11,29 @@ export const TIE_TYPES = {
   NAV: 'NAV',
 };
 
+export const getTieDimensions = (type) => {
+  switch (type) {
+    case TIE_TYPES.NAV:
+      return { height: 130, marginTop: 8, marginBottom: 8 };
+    case TIE_TYPES.TIME:
+      return { height: 8, marginTop: 2, marginBottom: 2 };
+    case TIE_TYPES.LOCATION:
+      return { height: 28, marginTop: 5, marginBottom: 5 };
+    case TIE_TYPES.ROUTE:
+      return { height: 28, marginTop: 5, marginBottom: 5 };
+    default:
+      return { height: 8, marginTop: 2, marginBottom: 2 };
+  }
+};
+
 const tieStyling = {
   borderWidth: 1,
   borderColor: 'grey',
   width: 70,
-  height: 8,
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  marginTop: 2,
-  marginBottom: 2,
+  ...getTieDimensions(),
 };
 
 export function LocationTie(props) {
@@ -30,9 +43,7 @@ export function LocationTie(props) {
       style={{
         ...tieStyling,
         backgroundColor: isSelected ? 'green' : color,
-        height: 28,
-        marginTop: 5,
-        marginBottom: 5,
+        ...getTieDimensions(TIE_TYPES.LOCATION),
       }}>
       <Text>{formatSecondsToDisplay(runTime)}</Text>
     </View>
@@ -45,6 +56,7 @@ export function TimeTie(props) {
     <View
       style={{
         ...tieStyling,
+        ...getTieDimensions(TIE_TYPES.TIME),
         backgroundColor: isSelected ? 'green' : color,
       }}
     />
@@ -59,10 +71,8 @@ export function NavTie(props) {
     <View
       style={{
         ...tieStyling,
-        margin: 0,
-        marginVertical: 0,
         width: 340,
-        height: 130,
+        ...getTieDimensions(TIE_TYPES.NAV),
         backgroundColor: color || phaseColor,
       }}>
       <Grid>
@@ -76,3 +86,65 @@ export function NavTie(props) {
     </View>
   );
 }
+
+export const renderTie = ({
+  item,
+  trackDisplay,
+  checkIfSelected,
+  localTime,
+}) => {
+  switch (trackDisplay) {
+    case TIE_TYPES.TIME:
+      const tracks = [];
+      for (let i = 0; i < item.runTime; i += 300) {
+        tracks.push(
+          <TimeTie
+            key={`${item.id}_${i}`}
+            isSelected={checkIfSelected(item.id)}
+            id={item.id}
+            name={item.name}
+            color={item.color}
+          />,
+        );
+      }
+      return <View>{tracks}</View>;
+    case TIE_TYPES.LOCATION:
+      return (
+        <LocationTie
+          key={`${item.id}`}
+          localTime={localTime}
+          isSelected={checkIfSelected(item.id)}
+          id={item.id}
+          name={item.name}
+          color={item.color}
+          runTime={item.runTime}
+        />
+      );
+    case TIE_TYPES.NAV:
+      return (
+        <NavTie
+          name={item.name}
+          floorState={item.floorState}
+          phase={item.phase}
+          phaseColor={item.phaseColor}
+          id={item.id}
+          color={item.color}
+          isSelected={checkIfSelected(item.id)}
+        />
+      );
+
+    //default is location
+    default:
+      return (
+        <LocationTie
+          key={`${item.id}`}
+          localTime={localTime}
+          isSelected={checkIfSelected(item.id)}
+          id={item.id}
+          name={item.name}
+          color={item.color}
+          runTime={item.runTime}
+        />
+      );
+  }
+};

2e6c3f92947115c9d7fbd2959ba04a516981cb12
Kalju Lee <kaljuvlee@gmail.com>
2021-05-05 15:18:55 -0400

    [dev]: blank component template

---
+++ src/helpers/ConnectedComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, Button, Container, Card, CardItem, Footer } from 'native-base';
+import { Grid, Col, Row } from 'react-native-easy-grid';
+import { connect } from 'react-redux';
+
+function CC(props) {}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+CC.defaultProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapStateToProps,
+)(CC);

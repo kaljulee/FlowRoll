b46e88c150325927f55647a10c11d91dea3583fb
Kalju Lee <kaljuvlee@gmail.com>
2021-05-06 17:12:39 -0400

    [control]: integerinput component

---
+++ src/pages/TrainScheduler/IntegerInput.js
@@ -0,0 +1,21 @@
+import { Col, Grid } from 'react-native-easy-grid';
+import NumberInput from '../../components/Inputs/NumberInput';
+import { Text } from 'native-base';
+import React from 'react';
+
+// todo should enforce integerness of input
+function IntegerInput(props) {
+  const { label, value, onValueChange } = props;
+  return (
+    <Grid style={{ width: '100%', height: '100%' }}>
+      <Col>
+        <NumberInput value={value} onValueChange={onValueChange} />
+      </Col>
+      <Col style={{ justifyContent: 'center' }}>
+        <Text style={{ fontSize: 10, textAlign: 'center' }}>{label}</Text>
+      </Col>
+    </Grid>
+  );
+}
+
+export default IntegerInput;
